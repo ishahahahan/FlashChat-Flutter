9d561435b7a8ba5163a690b0f622f7892183a3a8
ForsythIshan <ishand1005@gmail.com>
2023-07-16 14:20:17 +0530

    Commit with Firebase

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chat_app/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const WelcomeScreen(),
+        initialRoute: WelcomeScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => const WelcomeScreen(),
+          LoginScreen.id: (context) => const LoginScreen(),
+          RegistrationScreen.id: (context) => const RegistrationScreen(),
+          ChatScreen.id: (context) => const ChatScreen(),
+        });
+  }
+}
